839339b9e3417c71c9b10fb3049d73212ba8c240
kylebaker616 <kylebaker616@gmail.com>
2021-09-02 17:14:01 -0400

    Add secure resource request verification

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,3 +1,3 @@
-const apiUrl = 'http://api.openweathermap.org/data/2.5'
+const apiUrl = 'https://api.openweathermap.org/data/2.5'
 const apiKey = 'c1d51f580fdc52b711600df2be150360'
 export { apiUrl, apiKey }
